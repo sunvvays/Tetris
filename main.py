8f0f196f416b30a495557ed6152066b962442527
sunways <sashaosk2006@gmail.com>
2024-02-15 17:34:28 +0700

    Collision of shapes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from copy import deepcopy
+from random import choice, randrange
 
 
 W, H = 10, 20
@@ -23,13 +24,17 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 
 figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
 
 anim_count, anim_speed, anim_limit = 0, 60, 2000
 
-figure = deepcopy(figures[3])
+figure = deepcopy(choice(figures))
+
 
 def check_borders():
     if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
         return False
     return True
 
@@ -49,7 +54,7 @@ while True:
             elif event.key == pygame.K_DOWN:
                 anim_limit = 100
 
-    figure_old = deepcopy(figure)
+    figure_old = deepcopy(choice(figures))
     for i in range(4):
         figure[i].x += dx
         if not check_borders():
@@ -63,16 +68,23 @@ while True:
         for i in range(4):
             figure[i].y += 1
             if not check_borders():
+                for i in range(4):
+                    field[figure_old[i].y][figure_old[i].x] = pygame.Color("white")
                 figure = deepcopy(figure_old)
                 anim_limit = 2000
                 break
-
     [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
 
     for i in range(4):
         figure_rect.x = figure[i].x * TILE
         figure_rect.y = figure[i].y * TILE
         pygame.draw.rect(game_sc, pygame.Color("white"), figure_rect)
+
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                figure_rect.x, figure_rect.y = x * TILE, y * TILE
+                pygame.draw.rect(game_sc, col, figure_rect)
 
     pygame.display.flip()
     clock.tick(FPS)
