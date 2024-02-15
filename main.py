ccf5824c3a3d607dedf1237cff2f776616ef1d64
sunways <sashaosk2006@gmail.com>
2024-02-15 15:21:26 +0700

    The playing field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 W, H = 10, 20
 TILE = 45
 GAME_RES = W * TILE, H * TILE
@@ -9,12 +10,16 @@ pygame.init()
 game_sc = pygame.display.set_mode(GAME_RES)
 clock = pygame.time.Clock()
 
+grid = [pygame.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(W) for y in range(H)]
+
 while True:
     game_sc.fill(pygame.Color('black'))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+    [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
 
     pygame.display.flip()
     clock.tick(FPS)
